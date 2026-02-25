6bd0bb7a0147ff1bcf2a338f40cd2307652f3051
yussssra <yusra.here77@gmail.com>
2026-02-26 02:07:47 +0530

    feat: Add community and style slogans to marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,9 @@ import './Marquee.css';
 const Marquee = () => {
     const slogans = [
         "GULIM - THE BASIC STORE",
+        "YOUR STYLE IS OUR CONCERN",
         "PREMIUM DENIM FOR EVERYONE",
+        "SIGN IN TO BE PART OF THE GULIM COMMUNITY",
         "ELEVATE YOUR EVERYDAY STYLE",
         "QUALITY MEETS COMFORT",
         "GULIM - CRAFTED WITH CARE"
